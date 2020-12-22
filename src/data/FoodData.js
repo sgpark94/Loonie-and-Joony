ede707d678fdd416037b82f8545ad52148f98d04
Seulgi <olloyy@naver.com>
2020-12-22 21:27:13 +0900

    imageURL 링크로 삽입

--- src/data/FoodData.js
+++ src/data/FoodData.js
@@ -9,8 +9,8 @@ export default {
 				{
 					name: "Lunch",
 					content: "in Outback Steak-House",
-					imageURL: "@/assets/images/main0.jpg",
-					is_recipe: false
+					imageURL: "https://t1.daumcdn.net/cfile/tistory/27643838519B97FE07",
+					is_recipe: false,
 				},
 			],
 		},
@@ -20,16 +20,18 @@ export default {
 				{
 					name: "Lunch",
 					content: "near Sanghyeon Station",
-					imageURL: "@/assets/images/main1.jpg",
-					is_recipe: false
+					imageURL:
+						"https://lh3.googleusercontent.com/proxy/nzG1lssu7fsThl6bdrU7Mqd4R08YYpOFmnsBqng8W7Xi_j_FNCMjjRbfuLra8CKIP3-vn9uYiIWvjBoG8UGH2VDgde9Rkv3pWKqfVDy0wk1Dh5aORrTcJsHx9MaOY5EeDgQorxCQuplEPbeTb8cAnDisyEe4u_PTn_Br8vyHuEVColCvxnaSFysQDLbjinl0-VMass0qaCbXn9iu8-fAgtO9IUVmJvWQt9T3m40--PF8QoexQ_W1IZYxd8OsGtVQKue890Ba3Xjws5FEBJOkCUhF1DVASM0rwTzULt-5KlQZZRej1bFQNzmpBMAjIB1WG7JeM-BHJfKICEeMQEIIfywYX2anPGSD8j250UqzNcKtq5-TcBaY5-iHqsHNu9Q6ucmX4NVynv7imMtrdU4p09J7Ki5xDXvMt0JS85pxtyVDPo41toWeUenSqDcwHwxG49tokfLqEIE_hmsZt2RHTX9WhZkWVBtuxw9N6nMVQzp5iis1PJ_ZgwoBdAMZOYjlf6MHUIpkzsg5GHZvEC-OYXhDCyoimsPHqYoBjsm1V0UDj_LpHMoL8W8YMRW2MFjwKQxiTJ_07hbtAsBxQ799GLUoc1gsr51eO567kLpV",
+					is_recipe: false,
 				},
 				{
 					name: "Dinner",
 					content: "Grilled brazier",
-					imageURL: "@/assets/images/main2.jpg",
+					imageURL:
+						"https://woman.chosun.com/up_fd/wc_news/2017-02/bimg_org/1702_238.jpg",
 					is_recipe: true,
 					recipe:
-						"⛽연료, \n🧫화로, 🍆가지, 양파, 마늘, 🍄양송이, 🍍파인애플, 🥩고기, 🥂담금주",
+						"장비 : ⛽연료, 🧫화로 재료 : d🍆가지, 양파, 마늘, 🍄양송이, 🍍파인애플, 🥩고기, 🥂담금주",
 				},
 			],
 		},
@@ -39,21 +41,23 @@ export default {
 				{
 					name: "Breakfast",
 					content: "Branch",
-					imageURL: "@/assets/images/main3.jpg",
+					imageURL:
+						"https://post-phinf.pstatic.net/MjAxNzA3MTFfMTA0/MDAxNDk5NzUyNTQ5NzUy.rT1HxpNd3vwvKAMYRKLHjkxiv3D9ymwnHazL2Uf9JKkg.qz5gwLSeDgHluv0xmg95BhD9NYKCbdaN9aQwunYrN1gg.JPEG/GettyImages-467387974.jpg?type=w1200",
 					is_recipe: true,
 					recipe: "🍳달걀, 🥛우유, 🥖빵, 🥓베이컨",
 				},
 				{
 					name: "Lunch",
 					content: "Gambas, Alio Olio (Pasta)",
-					imageURL: "@/assets/images/main4.jpg",
-					is_recipe: true
+					imageURL: "https://i.ytimg.com/vi/9rSaUsreHz0/maxresdefault.jpg",
+					is_recipe: true,
 				},
 				{
 					name: "Dinner",
 					content: "Korea pancake (kimchi, mushroom, etc)",
-					imageURL: "@/assets/images/main5.jpg",
-					is_recipe: true
+					imageURL:
+						"https://www.icoop.or.kr/coopmall/goodimages/24909L0200_SUB3.jpg",
+					is_recipe: true,
 				},
 			],
 		},
@@ -63,14 +67,16 @@ export default {
 				{
 					name: "Breakfast",
 					content: "Branch",
-					imageURL: "@/assets/images/main6.jpg",
-					is_recipe: true
+					imageURL:
+						"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfdp23r5-GJNiJjroHVycBpXz0hAYUN1B8Ew&usqp=CAU",
+					is_recipe: true,
 				},
 				{
 					name: "Lunch",
 					content: "Stir-fried spicy pork + Ssambap",
-					imageURL: "@/assets/images/main7.jpg",
-					is_recipe: true
+					imageURL:
+						"https://mblogthumb-phinf.pstatic.net/MjAxODAxMTVfMjM2/MDAxNTE2MDI0Njk1NjY5.Ns3j25ISDWIfSKKLWaCBRHB9d3beyNLdXkJcZJfYrFcg.26RI4PGUDf_pnl6dNfW-vmU3vcvsh5e4-kjsPWHyfDwg.JPEG.yesica00/IMG_2623.JPG?type=w800",
+					is_recipe: true,
 				},
 			],
 		},
